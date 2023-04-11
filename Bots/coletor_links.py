3b5ad37335c5b11c99327ef611d8ab2eba9eb8dc
Fernando Linguanotti <101909952+fernandolf88@users.noreply.github.com>
2023-04-11 17:00:39 +0000

    Corrections

--- Bots/coletor_links.py
+++ Bots/coletor_links.py
@@ -54,10 +54,11 @@ websites = []
 
 for row in siteData:
 	websites.append(Website(row[0], row[1], row[2], row[3]))
-	crawler.parse(websites[0], 'http://shop.oreilly.com/product/0636920028154.do')
-	crawler.parse(websites[1], 'http://www.reuters.com/article/us-usa-epa-pruitt-idUSKBN19W2D0')
-	crawler.parse(websites[2], 'https://www.brookings.edu/blog/techtank/2016/03/01/idea-to-retire-old-methods-of-policy-education/')
-	crawler.parse(websites[3], 'https://www.nytimes.com/2018/01/28/business/energy-environment/oil-boom.html')
+
+crawler.parse(websites[0], 'http://shop.oreilly.com/product/0636920028154.do')
+crawler.parse(websites[1], 'http://www.reuters.com/article/us-usa-epa-pruitt-idUSKBN19W2D0')
+# crawler.parse(websites[2], 'https://www.brookings.edu/blog/techtank/2016/03/01/idea-to-retire-old-methods-of-policy-education/')
+crawler.parse(websites[3], 'https://www.nytimes.com/2018/01/28/business/energy-environment/oil-boom.html')
 
 
 
